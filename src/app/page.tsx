d34d8b7864b25e71082a1810baa7523c64e52d2a
nihalnaveed12 <nihalnaveed50@gmail.com>
2024-10-31 16:05:03 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,11 @@ export default function Home() {
   return (
     <main className="w-screen h-screen relative">
       <div
-        className="flex items-center w-full h-full bg-cover bg-center"
+        className="flex items-center w-full h-full bg-cover bg-center overflow-scroll"
         style={{ backgroundImage: "url(/main-bg.webp)" }}
       >
-        <div className="pl-20 md:pl-40 pb-56 md:pb-20 flex flex-col gap-5 z-[10] max-w-[750px]">
-          <h1 className="text-[50px] text-white font-semibold">
+        <div className="pl-10 md:pl-40 pb-56 md:pb-20 flex flex-col gap-5 z-[10] max-w-[750px] ">
+          <h1 className="text-[30px] md:text-[50px] text-white font-semibold">
             Make Anything possible with
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500">
               {" "}
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -30,8 +30,8 @@ export default function Navigation() {
 
   return (
     <div
-      style={{ left: "20%" }}
-      className="absolute z-[50] -bottom-20 w-[50%] md:w-[20%] max-h-[150px] rounded-full bg-black flex justify-between items-center border border-white py-7 px-4"
+      style={{ left: "12%" }}
+      className="absolute z-[50] -bottom-20 w-[50%] md:w-[20%] max-h-[150px] rounded-full bg-black flex justify-between items-center border border-white py-7 px-4 "
     >
       {isRouting && <Transition />}
       {NavLinks.map((nav) => (
